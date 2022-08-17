19e94569d0fb1e09ec756c175a25229bb72dccb0
kessio <64524758+kessio@users.noreply.github.com>
2022-08-17 09:04:48 +0300

    string greater than 1 and  less than 10

--- stringLength.js
+++ stringLength.js
@@ -1,7 +1,11 @@
 const stringLength = (string) => {
-   
+    if(string.lenght < 1){
+         return "Less than 1";
+     }else if(string.length > 10){
+         return "String should not be longer than 10 characters"
+    }else{
     return string.length;
-    
+    }
 }
 
 module.exports = stringLength;
--- stringLength.test.js
+++ stringLength.test.js
@@ -11,6 +11,35 @@ test('Get string length', () => {
     expect(typeof 'welcome').toBe('string');
   });
 
+  test('string length greater than 1', () => {
+    expect(stringLength('welcome')).toBeGreaterThan(1);
+})
+test('string length less than 10', () => {
+    expect(stringLength('welcome')).toBeLessThan(10);
+})
 
 
+ /* test('overcome is a string', () => {
+    expect(typeof 'overcome').toBe('string');
+  });
+
+  test('comedown is a string', () => {
+    expect(typeof 'comedown').toBe('string');
+  });
+  
+
+test('string length less than 10', () => {
+    const string = "welcome"
+    expect(string.length).toBeLessThan(10);
+})
+test('string length less than 10', () => {
+    const string = "overcome"
+    expect(string.length).toBeLessThan(10);
+})
+
+test('string length greater than 1', () => {
+    let string = "comedown"
+    expect(string.length).toBeGreaterThan(1);
+})
+  */
 
